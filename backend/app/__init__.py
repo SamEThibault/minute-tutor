00ad179608a16b4335492551a9713f37a2368e15
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-09-10 12:37:40 -0400

    endpoints developed

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,28 +1,52 @@
 from flask import Flask, request
 from dotenv import load_dotenv
 from flask_cors import CORS
+from app.db import User
 
 load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
 # check if pw matches db profile, if so, return status code
-@app.route("/signin", ["POST"])
+@app.route("/signin", methods=["POST"])
 def signin():
-    pass
+    name = request.form["name"]
+    password = request.form["password"]
+    user = User.get_or_none(User.username == name)
+
+    if user != None:
+        if user.password == password:
+            return {"body" : "Login successful", "status" : 200}
+        else:
+            return {"body" : "Incorrect password, please try again", "status" : 400}
+
+    return {"body" : "Invalid username or password, please try again", "status" : 400}
 
 # check if record exists, if not, create record in db
-@app.route("/signup", ["POST"])
+@app.route("/signup", methods=["POST"])
 def signup():
-    pass
+    try:
+        print(request)
+        name = request.form["name"]
+        password = request.form["password"]
+    except Exception as e:
+        print(e)
+        return {"body" : "Error", "status" : 400}
+
+    q = User.select().where(User.username == name)
+    # username must be unique, if it already exists, return error
+    if q.exists():
+        return {"body" : "User already exists!", "status" : 400}
+    User.create(username=name, password=password)
+    return {"body" : "Success", "status" : 200}
 
 # request contains updated user info, update profile in db 
-@app.route("/settings", ["POST"])
+@app.route("/settings", methods=["POST"])
 def settings():
     pass
 
 # receive question field, and return list of tutors that
 # are experts in that field
-@app.route("/tutors", ["POST"])
+@app.route("/tutors", methods=["POST"])
 def tutors():
     pass
---
+++ backend/app/db.py
@@ -0,0 +1,32 @@
+import os
+from peewee import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+print(db)
+
+class User(Model):
+    password = CharField()
+    username = CharField()
+    age = IntegerField(default=0)
+    zoomLink = CharField()
+    userType = CharField()
+    # for tags, it's all in a string, separated by commas
+    tags = CharField()
+    gender = CharField()
+    language = CharField()
+    Expertise = CharField()
+
+    class Meta:
+        database = db
+
+db.connect()
+db.create_tables([User])
