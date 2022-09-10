877e9dc702a1373d8f26d79a6288942e65d4be79
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-09-10 18:22:16 -0400

    initial setup

---
+++ frontend/src/components/TutorDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TutorDetails({username, zoomLink, tags, gender,
+                        language, expertise, email})
+{
+    return (
+        <div className="tutor-details-container">
+            
+        </div>
+    )
+}
