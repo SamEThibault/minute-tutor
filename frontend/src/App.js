e9d69a0e187f4b9bd63d24743d6b9487b10b9db4
Kevin <yu.kevin2002@gmail.com>
2022-09-10 13:56:09 -0700

    Laid out the skeleton of hte pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./styles/app.scss";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Tutors from "./pages/Tutors";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Signin />} />
+            <Route path="/tutors" element={<Tutors />} />
+            <Route path="/students" element={<Signin />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
