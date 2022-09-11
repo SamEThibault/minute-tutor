5ce2458a5a3416134c22a4b2d9b1923c3455d873
Kevin <yu.kevin2002@gmail.com>
2022-09-10 17:38:37 -0700

    Aded students

--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -1,37 +1,24 @@
-import {React, useState} from "react"
-import {useDispatch} from "react-redux";
+import { React, useState } from "react";
+import { useDispatch } from "react-redux";
 
-function Students()
-{
-    const dispatch = useDispatch();
-    const [subject, setSubject] = useState("");
-    const handleSubject = (subject) =>
-    {
-        setSubject(subject);
-    }
-    return (
-        <div className="students">
-            <div className="subject"
-            onClick={() => dispatch(handleSubject("computer science"))}>
-                Computer Science
-            </div>
-            <div className="subject">
-                Math
-            </div>
-            <div className="subject"
-            onClick={() => dispatch(handleSubject("life sciences"))}>
-                Life Sciences
-            </div>
-            <div className="subject"
-            onClick={() => dispatch(handleSubject("history"))}>
-                History
-            </div>
-            <div className="subject"
-            onClick={() => dispatch(handleSubject("biology"))}>
-                Biology
-            </div>
-        </div>
-    )
+function Students() {
+  const dispatch = useDispatch();
+  const [subject, setSubject] = useState("");
+  const handleSubject = (subject) => {
+    setSubject(subject);
+  };
+  return (
+    <div className="students col-c-c">
+      <h1>What subject would you like help in...</h1>
+      <div className="subject-filter col-c-fs">
+        {["computer science", "life sciences", "history", "biology"].map(
+          (subject) => (
+            <button>{subject}</button>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Students
+export default Students;
